9581ce87b0fae449b0fbdc8035d3fdc68cc5c395
Aleksandr <110897@mail.ru>
2022-07-20 19:38:22 +0300

    Еще один урок

--- script.js
+++ script.js
@@ -11,10 +11,31 @@ const personalMovieDB = {
     'privat': false
 }
 
-const lastMovie = prompt("Один из последних просмотренных фильмов?", '');
-const lastMovieRate = prompt('На сколько оцените его?', '');
 
 
-personalMovieDB.movies[lastMovie] = lastMovieRate;
+for (let i = 0; i < 2; i++) {
+    const lastMovie = prompt("Один из последних просмотренных фильмов?", '');
+    const lastMovieRate = prompt('На сколько оцените его?', '');
 
-console.log(personalMovieDB);
+    if (lastMovie != null && lastMovieRate != null && lastMovie != '' && lastMovieRate != '' && lastMovie.length < 50) {
+        personalMovieDB.movies[lastMovie] = lastMovieRate;
+    } else {
+        i--;
+    }
+}
+
+if (personalMovieDB.count < 10) {
+    console.log('Мало');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log('Норм');
+} else if (personalMovieDB.count >= 30) {
+    console.log('Киноман!');
+} else {
+    console.log('Ошибка');
+}
+
+
+
+
+
+
