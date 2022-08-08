732113f8583be33d29617bbb5d243cd72c53efbd
Aleksandr Dudnikov <110897@mail.ru>
2022-08-08 10:49:43 +0300

    обновление

--- home.js
+++ home.js
@@ -202,25 +202,148 @@
 
 // console.log(reverse(someString));
 
-const baseCurrencies = ['USD', 'EUR'];
-const additionalCurrencies = ['UAH', 'RUB', 'CNY'];
+// const baseCurrencies = ['USD', 'EUR'];
+// const additionalCurrencies = ['UAH', 'RUB', 'CNY'];
 
-function availableCurr(arr, missingCurr) {
-    let current = [...baseCurrencies, ...additionalCurrencies];
-    let str = 'Доступные валюты:\n';
-    if (current.length == 0) {
-        str = 'Нет доступных валют';
-    }
+// function availableCurr(arr, missingCurr) {
+//     let current = [...baseCurrencies, ...additionalCurrencies];
+//     let str = 'Доступные валюты:\n';
+//     if (current.length == 0) {
+//         str = 'Нет доступных валют\n';
+//     }
 
-    for (let key in current) {
-        if (current[key] == missingCurr) {
-            continue;
-        } else {
-            str += current[key] + '\n';
+//     for (let key in current) {
+//         if (current[key] == missingCurr) {
+//             continue;
+//         } else {
+//             str += current[key] + '\n';
+//         }
+
+//     }
+//     return str;
+// }
+
+// console.log(availableCurr(baseCurrencies));
+
+// const shoppingMallData = {
+//     shops: [
+//         {
+//             width: 10,
+//             length: 5
+//         },
+//         {
+//             width: 15,
+//             length: 7
+//         },
+//         {
+//             width: 20,
+//             length: 5
+//         },
+//         {
+//             width: 8,
+//             length: 10
+//         }
+//     ],
+//     height: 5,
+//     moneyPer1m3: 30,
+//     budget: 50000
+// }
+
+// function isBudgetEnough(data) {
+//     let square = 0;
+//     let volume = 0;
+
+//     data.shops.forEach(shop => {
+//         square += shop.width * shop.length;
+//     });
+
+//     volume = data.height * square;
+
+
+//     if (data.budget - (volume * data.moneyPer1m3) >= 0) {
+//         return 'Бюджета достаточно';
+//     } else {
+//         return 'Бюджета недостаточно';
+//     }
+
+    
+// }
+
+// console.log(isBudgetEnough(shoppingMallData));
+
+// const students = ['Peter', 'Andrew', 'Ann', 'Mark', 'Josh', 'Sandra', 'Cris', 'Bernard', 'Takesi', 'Sam'];
+
+// function sortStudentsByGroups(arr) {
+//     arr.sort();
+//     const a = [], b = [], c = [], rest = [];
+
+//     for (let i = 0; i < arr.length; i++) {
+//         if (i < 3) {
+//             a.push(arr[i]);
+            
+//         } else if (i < 6) {
+//             b.push(arr[i]);
+//         } else if (i < 9) {
+//             c.push(arr[i]);
+//         } else {
+//             rest.push(arr[i]);
+//         }
+//     }
+//     return [a,b,c, `Оставшиеся студенты: ${rest.length === 0 ? '-' : rest.join(', ')}`]
+// }
+
+// console.log(sortStudentsByGroups(students));
+
+const restorantData = {
+    menu: [
+        {
+            name: 'Salad Caesar',
+            price: '14$'
+        },
+        {
+            name: 'Pizza Diavola',
+            price: '9$'
+        },
+        {
+            name: 'Beefsteak',
+            price: '17$'
+        },
+        {
+            name: 'Napoleon',
+            price: '7$'
         }
+    ],
+    waitors: [
+        {name: 'Alice', age: 22}, {name: 'John', age: 24}
+    ],
+    averageLunchPrice: '20$',
+    openNow: true
+};
 
-    }
-    return str;
+function isOpen(prop) {
+    let answer = '';
+    prop ? answer = 'Открыто' : answer = 'Закрыто';
+
+    return answer;
 }
 
-console.log(availableCurr(baseCurrencies));
+console.log(isOpen(restorantData.openNow))
+
+function isAverageLunchPriceTrue(fDish, sDish, average) {
+    if ((+fDish.price.slice(0, 1) + +sDish.price.slice(0, 1)) < +average.slice(0, 2)) {
+        return 'Цена выше средней';
+    } else {
+        return 'Цена ниже средней';
+    }
+}
+
+console.log(isAverageLunchPriceTrue(restorantData.menu[1], restorantData.menu[3], restorantData.averageLunchPrice));
+console.log(restorantData);
+function transferWaitors(data) {
+    const copy = Object.assign({}, data);
+
+    copy.waitors = [{name: 'Mike', age: 32}];
+    return copy;
+}
+
+console.log(transferWaitors(restorantData));
--- script.js
+++ script.js
@@ -1,66 +1,67 @@
 'use strict';
 
-let numberOfFilms;
-
-function start() {
-    numberOfFilms = prompt('Сколько фильмов уже посмотрели?', '');
-    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
-        numberOfFilms = prompt('Сколько фильмов уже посмотрели?', '');
-    }
-}
-
-// start();
 
 const personalMovieDB = {
-    'count': numberOfFilms,
+    'count': 0,
     'movies': {},
     'actors': {},
     'genres': [],
-    'privat': false
-}
-
-function rememberMyFilms() {
-    for (let i = 0; i < 2; i++) {
-        const lastMovie = prompt("Один из последних просмотренных фильмов?", '').trim();
-        const lastMovieRate = prompt('На сколько оцените его?', '');
-
-        if (lastMovie != null && lastMovieRate != null && lastMovie != '' && lastMovieRate != '' && lastMovie.length < 50) {
-            personalMovieDB.movies[lastMovie] = lastMovieRate;
-        } else {
-            i--;
+    'privat': false,
+    start: function() {
+        personalMovieDB.count = prompt('Сколько фильмов уже посмотрели?', '');
+        while (personalMovieDB.count == '' || personalMovieDB.count == null || isNaN(personalMovieDB.count)) {
+            personalMovieDB.count = prompt('Сколько фильмов уже посмотрели?', '');
         }
+    },
+    rememberMyFilms: function() {
+        for (let i = 0; i < 2; i++) {
+            const lastMovie = prompt("Один из последних просмотренных фильмов?", '').trim();
+            const lastMovieRate = prompt('На сколько оцените его?', '');
+    
+            if (lastMovie != null && lastMovieRate != null && lastMovie != '' && lastMovieRate != '' && lastMovie.length < 50) {
+                personalMovieDB.movies[lastMovie] = lastMovieRate;
+            } else {
+                i--;
+            }
+        }
+    },
+    detectPersonalLevel: function() {
+        if (personalMovieDB.count < 10) {
+            console.log('Мало');
+        } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+            console.log('Норм');
+        } else if (personalMovieDB.count >= 30) {
+            console.log('Киноман!');
+        } else {
+            console.log('Ошибка');
+        }
+    }, 
+    showMyBD: function(hidden) {
+        if (!hidden) {
+            console.log(personalMovieDB);
+        }
+    }, 
+    toggleVisibleMyBD: function() {
+        if (personalMovieDB.privat) {
+            personalMovieDB = false;
+        } else {
+            personalMovieDB = true;
+        }
+    },
+    writeYourGenerals: function() {
+        for (let i = 1; i <= 3; i++) {
+            let genre = prompt(`Ваш любимый жанр ${i}`);
+
+            if (genre === '' || genre === null) {
+                console.log('Вы ввели некорректные данные или не ввели их вовсе');
+                i--;
+            } else {
+                personalMovieDB.genres[i - 1] =  genre;
+            }
+        }
+
+        personalMovieDB.genres.forEach((item, i) => {
+            console.log(`Любимый жанр ${i + 1} - это ${item}`);
+        })
     }
-}
-
-// rememberMyFilms();
-
-function detectPersonalLevel() {
-    if (personalMovieDB.count < 10) {
-        console.log('Мало');
-    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-        console.log('Норм');
-    } else if (personalMovieDB.count >= 30) {
-        console.log('Киноман!');
-    } else {
-        console.log('Ошибка');
-    }
-}
-
-detectPersonalLevel();
-
-function showMyBD(hidden) {
-    if (!hidden) {
-        console.log(personalMovieDB);
-    }
-}
-
-showMyBD(personalMovieDB.privat);
-
-function writeYourGenerals() {
-    for (let i = 1; i <= 3; i++) {
-        personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр ${i}`);
-    }
-}
-
-writeYourGenerals();
-
+}   
